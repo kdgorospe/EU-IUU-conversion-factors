f09b10e617dab1a7b287639d1c16fb30c91d80dd
Kelvin Gorospe <kdgorospe@gmail.com>
2020-07-20 14:41:22 -0400

    recode landings data

--- explore_landings.R
+++ explore_landings.R
@@ -37,8 +37,9 @@ landings_dat_clean <- landings_dat %>%
   mutate_all(function(x) gsub(pattern = " e| c| d| p", replacement = "", x)) %>% # Remove data flags
   mutate_all(function(x) gsub(pattern = ":", replacement = NA, x)) %>% # Replace with NAs
   mutate_at(-c(1), as.numeric) %>%
-  separate(col = names(landings_dat)[1], into = c("species", "pres", "dest_use", "unit", "natvessr", "geo"), sep = ",") # Split first column
+  separate(col = names(landings_dat)[1], into = c("species", "pres", "dest_use", "unit", "natvessr", "geo"), sep = ",") 
 
+  
 codesdir <- file.path("Data", "Codes")
 code_files <- file.path(codesdir, list.files(codesdir))
 
@@ -53,7 +54,51 @@ no_filetype <- lapply(code_files, function(i){strsplit(i, "\\.")[[1]][1]})
 just_filename <- lapply(no_filetype, function(i){strsplit(i, "\\/")[[1]][3]})
 names(code_key) <- unlist(just_filename)
 
+# Create named character vectors (Code = Translation) using code_key
+#code_key_vec <- code_key$dest_use$Translation
+#names(code_key_vec) <- code_key$dest_use$Code
+code_key_vec <- list()
+for (i in 1:length(code_key)){
+  code_key_vec[[i]] <- code_key[[i]]$Translation
+  names(code_key_vec[[i]]) <- code_key[[i]]$Code
+  names(code_key_vec)[i] <- names(code_key)[i]
+}
+
+# Fix "NA" in geo (was read in as missing data NA, but the code is "NA" for Namibia)
+code_key_vec$geo[3563]
+names(code_key_vec$geo)[is.na(names(code_key_vec$geo))]<-"NAM"
+# FIX IT - Make sure NA is replaced with NAM in raw data
+
+# Fix "NA" in natvessr - same problem as with geo
+code_key_vec$natvessr[207]
+names(code_key_vec$natvessr)[is.na(names(code_key_vec$natvessr))]<-"NAM"
+
+if (sum(is.na(landings_dat_clean$geo))>0){
+  warning("Inspect 'geo' column for <NA> vs Namibia")
+}
+
+if (sum(is.na(landings_dat_clean$natvessr))>0){
+  warning("Inspect 'natvessr' column for <NA> vs Namibia")
+}
+
+# Use named character vector to replace codes in landings_dat_clean with translations:
+landings_dat_coded <- as.data.frame(landings_dat_clean) %>%
+  mutate(dest_use = recode(dest_use, !!!code_key_vec$dest_use)) %>%
+  mutate(geo = recode(geo, !!!code_key_vec$geo)) %>%
+  mutate(natvessr = recode(natvessr, !!!code_key_vec$natvessr)) %>%
+  mutate(pres = recode(pres, !!!code_key_vec$pres)) %>%
+  mutate(species = recode(species, !!!code_key_vec$species)) %>%
+  mutate(unit = recode(unit, !!!code_key_vec$unit)) %>%
+  separate(col = species, into = c("common_name", "scientific_name"), sep = " - ", fill = "right") %>% # fill = right; i.e., if scientific name is missing, fill column on the right with NA
+  rename(presentation = pres,
+         use = dest_use,
+         nationality_of_vessel = natvessr,
+         reporting_entity = geo)
+
+ 
+
 
 # Replace with meaningful column names: "species", "presentation", "use", "unit", "nationality_of_vessel", "reporting_entity"
+
 
                            
